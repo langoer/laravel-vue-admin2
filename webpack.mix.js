cfa9bf7330a6ddf03dabdb0c3736944e8eb7ab18
langoer <langoer@gmail.com>
2020-09-18 19:56:17 +0800

    init

---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+const mix = require("laravel-mix");
+
+
+
+mix.config.webpackConfig = {
+  output: {
+    publicPath: "/vendor/laravel-vue-admin/",
+  }
+};
+
+
+mix
+  .js("./resources/js/app.js", "public")
+  .extract(["axios", "vue", "vuex", "vue-router", "element-ui"])
+  .setResourceRoot("/vendor/laravel-vue-admin")
+  .setPublicPath("public")
+  .copy("public", "../public/vendor/laravel-vue-admin")
+  .webpackConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "resources/js/"),
+      },
+    },
+  })
+  .options({
+    extractVueStyles: false,
+    processCssUrls: false,
+  })
+  .disableNotifications().version();
